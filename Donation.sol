c008d66c11ead656dd3d711d7a228ef6438c8696
hazae41 <hazae41@gmail.com>
2021-06-11 15:18:52 +0200

    donation

--- Donation.sol
+++ Donation.sol
@@ -4,25 +4,26 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Donation {
     IERC20 public token;
-    address oracle;
+    address payable oracle;
     
-    constructor(address _token, address _oracle){
+    constructor(address _token, address payable _oracle){
         token = IERC20(_token);
         oracle = _oracle;
     }
     
     mapping (string => uint) public pendings;
     
-    event Transfer(string indexed uri, uint amount, uint gas);
+    event Transfer(address indexed sender, string uri, uint amount, uint gas);
     
     /**
      * Called by the user
      **/
     function transfer(string memory uri, uint amount) external payable {
         require(msg.value > 0);
-        require(token.transferFrom(msg.sender, oracle, amount));
-        emit Transfer(uri, amount, msg.value);
-        pendings[uri] = amount;
+        require(token.transferFrom(msg.sender, address(this), amount));
+        emit Transfer(msg.sender, uri, amount, msg.value);
+        oracle.transfer(msg.value);
+        pendings[uri] += amount;
     }
     
     /**
@@ -32,7 +33,7 @@ contract Donation {
         require(msg.sender == oracle);
         uint amount = pendings[uri];
         require(amount > 0);
-        require(token.transferFrom(oracle, target, amount));
+        require(token.transfer(target, amount));
         pendings[uri] = 0;
     }
 }
