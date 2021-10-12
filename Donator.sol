761d65bc07e7a5bc6b49a5ee3241ec5c4760acc2
hazae41 <hazae41@gmail.com>
2021-10-12 23:18:33 +0200

    final

--- Donator.sol
+++ Donator.sol
@@ -3,11 +3,21 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Donator {
-    event Donation(IERC20 indexed token, address indexed sender, address indexed receiver, string uri, uint amount);
+    IERC20 private $token;
     
-    function donate(IERC20 token, address payable receiver, string memory uri, uint amount) external {
-        require(amount > 0);
-        require(token.transferFrom(msg.sender, receiver, amount));
-        emit Donation(token, msg.sender, receiver, uri, amount);
+    constructor(IERC20 _token){
+        $token = _token;
+    }
+    
+    function token() external view returns (IERC20) {
+        return $token;
+    }
+    
+    event Donation(address indexed sender, address indexed receiver, string uri, uint amount);
+    
+    function donate(address payable receiver, string memory uri, uint amount) external {
+        require(amount > 0, "Donator: invalid amount");
+        require($token.transferFrom(msg.sender, receiver, amount));
+        emit Donation(msg.sender, receiver, uri, amount);
     }
 }
--- Property.sol
+++ Property.sol
@@ -11,33 +11,22 @@ contract Factory {
         $token = _token;
     }
     
-    /**
-     * @dev Get the token
-     */
     function token() external view returns (ERC20) {
         return $token;
     }
     
-    event Create(string indexed name, address indexed owner);
+    event Create(string indexed name, address indexed creator);
     
-    /**
-     * @dev Create a new property
-     **/
     function create(string calldata _name) external returns (Property) {
         require($properties[_name] == Property(address(0)));
         
-        string memory _symbol = string(abi.encodePacked($token.symbol(),".", _name));
-        
-        Property _property = new Property(_name, _symbol, msg.sender, $token);
+        Property _property = new Property(_name, msg.sender, $token);
         $properties[_name] = _property;
         emit Create(_name, msg.sender);
         
         return _property;
     }
     
-    /**
-     * @dev Retrieve an existing property
-     **/
     function get(string calldata _name) external view returns (Property) {
         Property property = $properties[_name];
         require(address(property) != address(0));
@@ -51,17 +40,13 @@ contract Property is ERC20 {
     
     constructor(
         string memory _name,
-        string memory _symbol,
         address _owner,
         ERC20 _token
-    ) ERC20(_name, _symbol) {
+    ) ERC20(_name, $token.symbol()) {
         $token = _token;
         $owner = _owner;
     }
     
-    /**
-     * @dev Returns the current owner
-     */
     function owner() external view returns (address) {
         return $owner;
     }
@@ -72,6 +57,7 @@ contract Property is ERC20 {
      * @dev Deposit some tokens and increase the price
      **/
     function deposit(uint _amount) external {
+        require(_amount > 0, "Property: invalid amount");
         require($token.transferFrom(msg.sender, address(this), _amount));
         _mint(msg.sender, _amount);
         emit Deposit(msg.sender, _amount);
@@ -83,12 +69,13 @@ contract Property is ERC20 {
      * @dev Withdraw some tokens and decrease the price
      **/
     function withdraw(uint _amount) external {
+        require(_amount > 0, "Property: invalid amount");
         require($token.transfer(msg.sender, _amount));
         _burn(msg.sender, _amount);
         emit Withdraw(msg.sender, _amount);
     }
     
-    event Buy(address indexed oldOwner, address indexed newOwner);
+    event Buy(address indexed previous, address indexed next);
     
     /**
      * @dev Buy the property at the current price
@@ -101,7 +88,7 @@ contract Property is ERC20 {
         $owner = msg.sender;
     }
     
-    event Send(address indexed oldOwner, address indexed newOwner);
+    event Send(address indexed previous, address indexed next);
     
     /**
      * @dev Send the property to someone
